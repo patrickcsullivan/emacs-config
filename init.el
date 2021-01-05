61eda7eb4856c23c804e38c40a8929592f4a3e74
Patrick Sullivan <patricksullivan08@gmail.com>
2021-01-05 09:48:47 -0500

    config

--- init.el
+++ init.el
@@ -1,32 +1,69 @@
-;; Load non-default themes.
+;; --------------------------------------------------------------------
+;; ## Set up package management
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+(require 'package)     ; Bring in package functions.
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("gnu" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
 
-;;(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-;;(load-theme 'dracula t)
+;; Refresh package contents on new system loads.
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Import use-package.
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package):
+(setq use-package-always-ensure t)
+
+;; --------------------------------------------------------------------
+;; ## Packages
+
+(use-package command-log-mode)          ; To use:
+					; M-x globalcommand-log-mode
+					; M-x clm/toggle-command-log-buffer
+
+;; --------------------------------------------------------------------
+;; ## User interface
+
+;; Clean up Emacs UI.
+
+(setq inhibit-startup-message nil)
+(tool-bar-mode nil)  ; Disable the toolbar.
+(tooltip-mode nil)   ; Disable tooltips.
+
+(setq visible-bell t)               ; Turn on visible bell.
+(global-display-line-numbers-mode)  ; Always show line numbers.
+
+(setq blink-cursor-mode nil)  ; Don't blink cursor.
+(setq cursor-type 'bar)       ; Use bar cursor.
+
+;; Mode line
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
+
+;; --------------------------------------------------------------------
+;; Buffer completion framework
+
+;; Helm and Ivy are two main options
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(blink-cursor-mode nil)
- '(cursor-type (quote bar))
+ ;; If there is more than one, they won't work rig
+
  '(custom-enabled-themes (quote (dracula)))
  '(custom-safe-themes
    (quote
-    ("2dff5f0b44a9e6c8644b2159414af72261e38686072e063aa66ee98a2faecf0e" default)))
- '(menu-bar-mode t)
- '(package-archives
+    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "2dff5f0b44a9e6c8644b2159414af72261e38686072e063aa66ee98a2faecf0e" default)))
+ '(idris-interpreter-path "idris2")
+ '(package-selected-packages
    (quote
-    (("melpa" . "https://melpa.org/packages/")
-     ("gnu" . "https://elpa.gnu.org/packages/"))))
- '(package-selected-packages (quote (spacemacs-theme dracula-theme)))
- '(tool-bar-mode nil))
+    (company idris-mode spacemacs-theme afternoon-theme dracula-theme)))
+ )
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -36,5 +73,5 @@
  )
 
 ;; Always show line numbers.
-(global-display-line-numbers-mode)
+
 
