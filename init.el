6827a50518b151758fb7cfecf6624b05300249e2
Patrick Sullivan <patricksullivan08@gmail.com>
2021-01-07 16:58:39 -0500

    ...

--- init.el
+++ init.el
@@ -1,4 +1,9 @@
-;; --------------------------------------------------------------------
+;;; package --- Summary
+
+;;; Commentary:
+
+;;; Code:
+
 ;; ## Set up package management
 
 (require 'package)     ; Bring in package functions.
@@ -16,62 +21,66 @@
 (require 'use-package):
 (setq use-package-always-ensure t)
 
-;; --------------------------------------------------------------------
+;; ------------------------------------------------------------------------------
 ;; ## Packages
 
 (use-package command-log-mode)          ; To use:
 					; M-x globalcommand-log-mode
 					; M-x clm/toggle-command-log-buffer
 
-;; --------------------------------------------------------------------
+;; ------------------------------------------------------------------------------
 ;; ## User interface
 
-;; Clean up Emacs UI.
+;; ### Clean up Emacs UI.
 
-(setq inhibit-startup-message nil)
-(tool-bar-mode nil)  ; Disable the toolbar.
-(tooltip-mode nil)   ; Disable tooltips.
-
+(setq inhibit-startup-message nil)  ; Hide startup screen.
+(tool-bar-mode nil)                 ; Disable the toolbar.
+(tooltip-mode nil)                  ; Disable tooltips.
 (setq visible-bell t)               ; Turn on visible bell.
+
+;; ### Line and column numbers
+
+(column-number-mode)                ; Show column number in mode line.
 (global-display-line-numbers-mode)  ; Always show line numbers.
+
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		shell-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;; ### Cursor
 
 (setq blink-cursor-mode nil)  ; Don't blink cursor.
 (setq cursor-type 'bar)       ; Use bar cursor.
 
-;; Mode line
+;; ### Mode line
 
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1))
 
-;; --------------------------------------------------------------------
-;; Buffer completion framework
+;; ## Programming
 
-;; Helm and Ivy are two main options
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work rig
+;; Install company for autocompletion popup.
+(use-package company
+  :hook (prog-mode . company-mode))
 
- '(custom-enabled-themes (quote (dracula)))
- '(custom-safe-themes
-   (quote
-    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "2dff5f0b44a9e6c8644b2159414af72261e38686072e063aa66ee98a2faecf0e" default)))
- '(idris-interpreter-path "idris2")
- '(package-selected-packages
-   (quote
-    (company idris-mode spacemacs-theme afternoon-theme dracula-theme)))
- )
+;; Install flycheck for real-time syntax checking (squiggly line errors).
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; ## Help
 
-;; Always show line numbers.
+;; Show key-binding completion suggestions in a new buffer.
+(use-package which-key
+  :init (which-key-mode))
 
+(provide 'init)
+;;; init.el
 
