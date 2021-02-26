6b476a3e6c052c473f09fab6a30402b0c407b61a
Patrick Sullivan <patrick.sullivan@fastradius.com>
2021-02-26 14:06:35 -0500

    wip

--- init.el
+++ init.el
@@ -1,86 +1,38 @@
-;;; package --- Summary
+(setq inhibit-startup-message t)
 
-;;; Commentary:
+; Basic UI
+(tool-bar-mode 1)     ; Enable toolbar.
+(tooltip-mode 1)      ; Enable tooltips.
+(menu-bar-mode 1)     ; Enable menu bar.
+(set-fringe-mode 10)  ; Give some breathing room.
+(setq visible-bell t) ; Use visiual bell instead of audio.
 
-;;; Code:
+; Font, theme
+(set-face-attribute 'default nil :height 150)
+(load-theme 'tango-dark)
 
-;; ## Set up package management
+;; ----------
 
-(require 'package)     ; Bring in package functions.
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("gnu" . "https://elpa.gnu.org/packages/")))
+;; Initialize package sources.
+(require 'package)
+(setq package-archives `(("melpa" . "https://melp.org/packages/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
 
-;; Refresh package contents on new system loads.
+; Refresh package contents on fresh system.
 (unless package-archive-contents
   (package-refresh-contents))
 
-;; Import use-package.
+; Initialize use-package on non-Linux platforms.
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
-(require 'use-package):
+
+(require 'use-package)
 (setq use-package-always-ensure t)
 
-;; ------------------------------------------------------------------------------
-;; ## Packages
+;; ----------
 
-(use-package command-log-mode)          ; To use:
-					; M-x globalcommand-log-mode
-					; M-x clm/toggle-command-log-buffer
-
-;; ------------------------------------------------------------------------------
-;; ## User interface
-
-;; ### Clean up Emacs UI.
-
-(setq inhibit-startup-message nil)  ; Hide startup screen.
-(tool-bar-mode nil)                 ; Disable the toolbar.
-(tooltip-mode nil)                  ; Disable tooltips.
-(setq visible-bell t)               ; Turn on visible bell.
-
-;; ### Line and column numbers
-
-(column-number-mode)                ; Show column number in mode line.
-(global-display-line-numbers-mode)  ; Always show line numbers.
-
-;; Disable line numbers for some modes
-(dolist (mode '(org-mode-hook
-		term-mode-hook
-		shell-mode-hook
-		eshell-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
-
-;; ### Cursor
-
-(setq blink-cursor-mode nil)  ; Don't blink cursor.
-(setq cursor-type 'bar)       ; Use bar cursor.
-
-;; ### Mode line
-
-(use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1))
-
-;; ## Programming
-
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
-
-;; Install company for autocompletion popup.
-(use-package company
-  :hook (prog-mode . company-mode))
-
-;; Install flycheck for real-time syntax checking (squiggly line errors).
-(use-package flycheck
-  :ensure t
-  :init (global-flycheck-mode))
-
-;; ## Help
-
-;; Show key-binding completion suggestions in a new buffer.
-(use-package which-key
-  :init (which-key-mode))
-
-(provide 'init)
-;;; init.el
+; Run using `M-x global-commnad-log-mode`
+; Then `M-x clm/toggle-command-log-buffer`
+(use-package command-log-mode)  
 
