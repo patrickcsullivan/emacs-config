d578e97bb6d7d005762103aee7636f33bdd4bd87
Patrick Sullivan <patrick.sullivan@fastradius.com>
2021-02-26 16:47:36 -0500

    wip

--- init.el
+++ init.el
@@ -13,10 +13,12 @@
 
 ;; ----------
 
+;; PACKAGE INIT
+
 ;; Initialize package sources.
 (require 'package)
-(setq package-archives `(("melpa" . "https://melp.org/packages/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
+;(add-to-list 'package-archives `("melpa" . "https://melp.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
 ; Refresh package contents on fresh system.
@@ -32,7 +34,55 @@
 
 ;; ----------
 
-; Run using `M-x global-commnad-log-mode`
-; Then `M-x clm/toggle-command-log-buffer`
-(use-package command-log-mode)  
+;; THEME
 
+(use-package doom-themes
+  :ensure t
+  :config
+  (load-theme 'doom-one t))
+
+;; ----------
+
+;; IDE
+
+;; TODO
+;; - Company: buffer completion
+;; - Projectile: projects
+;; - Theme
+;; - Flycheck: syntax checking
+
+;; Run using `M-x global-commnad-log-mode`
+;; Then `M-x clm/toggle-command-log-buffer`
+(use-package command-log-mode)
+
+; Sidebar navigation with extras
+(use-package treemacs
+  :ensure t
+  :bind
+  (:map global-map
+	([f8] . treemacs))
+  :config
+  (progn
+    (setq treemacs-is-never-other-window t))
+  (treemacs-filewatch-mode t))
+
+; ----------
+
+(use-package elixir-mode
+  :ensure t)
+
+; ----------
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(doom-themes elixir-mode treemacs-magit treemacs-projectile use-package treemacs rainbow-delimiters magit flycheck doom-modeline company command-log-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
